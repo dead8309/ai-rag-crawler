c9f79232f86ff6d3fd09f698b158a686411b5b12
Vaibhav Raj <vaibhavmonu8309@gmail.com>
2024-12-14 22:46:43 +0530

    cleanup api routes
    - remove unused routes

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,8 @@ import { instrument } from "@fiberplane/hono-otel";
 import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 import { Hono } from "hono";
-import { users, pages, sites } from "./db/schema";
+import { pages, sites } from "./db/schema";
 import { and, cosineDistance, desc, eq, gt, sql as rawSql } from "drizzle-orm";
-import { scrapeSite } from "./scrape";
 import { AI_MODELS, SYSTEM_PROMPT } from "./constants";
 import { RagWorflowParams, RagWorkflow } from "./workflows/rag";
 
@@ -20,7 +19,6 @@ app.get("/", (c) => {
   return c.text("Honc! 🪿");
 });
 
-// URL curd
 app.get("/api/sites", async (c) => {
   const sql = neon(c.env.DATABASE_URL);
   const db = drizzle(sql);
@@ -29,54 +27,12 @@ app.get("/api/sites", async (c) => {
       id: sites.id,
       url: sites.url,
       totalPages: sites.totalPages,
+      createdAt: sites.createdAt,
+      updatedAt: sites.updatedAt,
     })
     .from(sites);
 
   return c.json(rows);
-});
-
-app.get("/api/sites/:siteId", async (c) => {
-  try {
-    const sql = neon(c.env.DATABASE_URL);
-    const db = drizzle(sql);
-    const siteId = Number.parseInt(c.req.param("siteId"));
-    const siteRows = await db.select().from(sites).where(eq(sites.id, siteId));
-    if (siteRows.length === 0) {
-      return c.json({ message: "Site not found" }, 404);
-    }
-    return c.json(siteRows);
-  } catch (error) {
-    console.error("Error fetching site", error);
-    return c.json({ message: "An error occured" }, 500);
-  }
-});
-
-app.get("/api/sites/:siteId/pages", async (c) => {
-  const sql = neon(c.env.DATABASE_URL);
-  const db = drizzle(sql);
-  const siteId = Number.parseInt(c.req.param("siteId"));
-
-  if (isNaN(siteId)) {
-    return c.json({ message: "Invalid siteId" }, 400);
-  }
-  try {
-    const sitePages = await db
-      .select({
-        id: pages.id,
-        title: pages.title,
-        url: pages.url,
-      })
-      .from(pages)
-      .where(eq(pages.siteId, siteId));
-    if (sitePages.length === 0) {
-      return c.json({ message: "No pages found" }, 404);
-    }
-
-    return c.json(sitePages);
-  } catch (error) {
-    console.error("Error fetching pages", error);
-    return c.json({ message: "An error occured" }, 500);
-  }
 });
 
 app.delete("/api/sites", async (c) => {
@@ -89,6 +45,43 @@ app.delete("/api/sites", async (c) => {
     return c.json({ message: "Site deleted" });
   } catch (error) {
     console.log("Error deleting site", error);
+    return c.json({ message: "An error occured" }, 500);
+  }
+});
+
+app.get("/api/sites/:siteId", async (c) => {
+  const sql = neon(c.env.DATABASE_URL);
+  const db = drizzle(sql);
+  const siteId = Number.parseInt(c.req.param("siteId"));
+
+  if (isNaN(siteId)) {
+    return c.json({ message: "Invalid siteId" }, 400);
+  }
+  try {
+    const [sitePages] = await db
+      .select({
+        siteId: sites.id,
+        siteUrl: sites.url,
+        totalPages: sites.totalPages,
+      })
+      .from(sites)
+      .where(eq(sites.id, siteId));
+
+    const pageRows = await db
+      .select({
+        id: pages.id,
+        title: pages.title,
+        url: pages.url,
+      })
+      .from(pages)
+      .where(eq(pages.siteId, siteId));
+
+    return c.json({
+      ...sitePages,
+      pages: pageRows,
+    });
+  } catch (error) {
+    console.error("Error fetching pages", error);
     return c.json({ message: "An error occured" }, 500);
   }
 });
@@ -135,47 +128,7 @@ app.delete("/api/pages/:pageId", async (c) => {
   }
 });
 
-app.post("/api/scrape/page", async (c) => {
-  const sql = neon(c.env.DATABASE_URL);
-  const db = drizzle(sql);
-  const { url, strict = "false", type = "browser" } = await c.req.json();
-  if (!url) {
-    return c.json({ message: "URL is required" }, 400);
-  }
-  try {
-    const existingPage = await db
-      .select({ id: pages.id })
-      .from(pages)
-      .where(eq(pages.url, url))
-      .limit(1);
-
-    if (existingPage.length > 0) {
-      return c.json({ message: "Page with this URL already exists" }, 400);
-    }
-
-    const { scrapedData, siteId, totalProcessed } = await scrapeSite({
-      db,
-      baseURL: url,
-      strict,
-      type,
-    });
-
-    await db
-      .update(sites)
-      .set({ totalPages: totalProcessed })
-      .where(eq(sites.id, siteId));
-
-    const result = scrapedData.map((data) => {
-      return { url: data.url, title: data.title };
-    });
-
-    return c.json({ siteId, result: result });
-  } catch (error) {
-    return c.json({ "An Error Occured": error }, 500);
-  }
-});
-
-app.post("/api/scrape/page/workflow", async (c) => {
+app.post("/api/scrape/workflow", async (c) => {
   const sql = neon(c.env.DATABASE_URL);
   const db = drizzle(sql);
   const { url, strict = "false", type = "browser" } = await c.req.json();
@@ -211,8 +164,8 @@ app.post("/api/scrape/page/workflow", async (c) => {
   }
 });
 
-app.post("/api/scrape/page/workflow/status", async (c) => {
-  const { id } = await c.req.json();
+app.get("/api/scrape/workflow/:id", async (c) => {
+  const id = c.req.param("id");
   try {
     let instance = await c.env.RAG_WORKFLOW.get(id);
     return Response.json({
@@ -223,45 +176,6 @@ app.post("/api/scrape/page/workflow/status", async (c) => {
     const msg = `failed to get instance ${id}: ${e.message}`;
     console.error(msg);
     return Response.json({ error: msg }, { status: 400 });
-  }
-});
-
-app.post("/api/pages/generate-embeddings", async (c) => {
-  const sql = neon(c.env.DATABASE_URL);
-  const db = drizzle(sql);
-  const { pageId } = await c.req.json();
-  if (!pageId) {
-    return c.json({ message: "Page ID is required" }, 400);
-  }
-  try {
-    const page = await db
-      .select({
-        title: pages.title,
-        text: pages.cleanedText,
-      })
-      .from(pages)
-      .where(eq(pages.id, pageId))
-      .limit(1);
-    if (page.length === 0) {
-      return c.json({ message: "Page not found" }, 404);
-    }
-    const { data } = await c.env.AI.run(AI_MODELS.embeddings, {
-      text: [page[0].title, page[0].text],
-    });
-    const values = data[0];
-    if (!values) {
-      return c.json({ message: "Embeddings not generated" }, 500);
-    }
-    await db
-      .update(pages)
-      .set({
-        embedding: values,
-      })
-      .where(eq(pages.id, pageId));
-
-    return c.json({ message: "Embeddings generated" });
-  } catch (error) {
-    return c.json({ "An Error Occured": error }, 500);
   }
 });
 
@@ -299,10 +213,7 @@ app.post("/api/sites/ask", async (c) => {
     const context =
       relevantContext.length > 0
         ? `Context:\n${relevantContext
-            .map(
-              (docs) =>
-                `Title: ${docs.title}\n URL: ${docs.url}\n Content: ${docs.content}\n`
-            )
+            .map((docs) => `${docs.url}\n\n${docs.title}\n\n${docs.content}\n`)
             .join("\n")}`
         : "";
     console.log(context);
@@ -310,7 +221,6 @@ app.post("/api/sites/ask", async (c) => {
     const { response } = (await c.env.AI.run(AI_MODELS.text_generation, {
       messages: [
         { role: "system", content: SYSTEM_PROMPT + context },
-        // { role: "system", content: context },
         { role: "user", content: question },
       ],
     })) as { response: string };
@@ -319,15 +229,6 @@ app.post("/api/sites/ask", async (c) => {
   } catch (error) {
     return c.json({ "An Error Occured": error }, 500);
   }
-});
-
-// Users CRUD
-app.get("/api/users", async (c) => {
-  const sql = neon(c.env.DATABASE_URL);
-  const db = drizzle(sql);
-  return c.json({
-    users: await db.select().from(users),
-  });
 });
 
 // TODO: Implement streaming and real-time updates

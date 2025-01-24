7a0350e9d4a96c4adce1df8540c4069cbdb4b911
Vaibhav Raj <vaibhavmonu8309@gmail.com>
2025-01-25 01:21:29 +0530

    log instead of throwing error

--- apps/web/lib/client.ts
+++ apps/web/lib/client.ts
@@ -2,7 +2,7 @@ import { AppType } from "@repo/api";
 import { hc } from "hono/client";
 
 if (!process.env.NEXT_PUBLIC_SERVER_URL) {
-  throw new Error("NEXT_PUBLIC_SERVER_URL is not defined");
+  console.error("NEXT_PUBLIC_SERVER_URL is not defined");
 }
 
-export const client = hc<AppType>(process.env.NEXT_PUBLIC_SERVER_URL as string);
+export const client = hc<AppType>(process.env.NEXT_PUBLIC_SERVER_URL || "");
